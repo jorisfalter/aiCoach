d04af46c9dd0634b07f0a11995b04339b8bad633
Joris Falter <joris@jorisfalter.com>
2024-02-28 10:48:27 +0700

    x

---
+++ README.md
@@ -0,0 +1,1 @@
+# aiCoach
---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from flask import Flask, send_from_directory
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+# Access the OpenAI API key
+openai_api_key = os.getenv('API_KEY')
+
+client = OpenAI(api_key=openai_api_key)
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+
+
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    user_message = request.json['message']
+    response = client.chat.completions.create(model="gpt-4",
+        messages=[
+            {"role": "system", "content": "you always answer with a joke"},
+            {"role": "user", "content": user_message}
+            ])
+    print(response)
+    return jsonify({'response': response.choices[0].message.content})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+@app.route('/')
+def home():
+    return send_from_directory('static', 'index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
