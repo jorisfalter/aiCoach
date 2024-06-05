e3009e03d71593a8a50db31ac908aeadb4b483cc
Joris Falter <joris@jorisfalter.com>
2024-06-05 19:54:03 +0700

    x

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
+from flask_socketio import SocketIO
 import os
 import io    
 from openai import OpenAI
 from dotenv import load_dotenv
-
+import requests
+import base64
 
 
 app = Flask(__name__)
@@ -12,7 +14,7 @@ cors = CORS(app, resources={r"/upload": {"origins": "*"}})  # Enable CORS for th
 load_dotenv()
 openai_api_key = os.getenv('API_KEY')
 client = OpenAI(api_key=openai_api_key)
-
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -56,6 +58,33 @@ def upload_audio():
         # # Extract bot response
         bot_response = response.choices[0].message.content
         print(bot_response)
+
+        # call elevenlabs to put it in Tony's voice
+        url = "https://api.elevenlabs.io/v1/text-to-speech/JqDxs5THf3pyDYeCJfCi"
+
+        payload = {
+            "text": bot_response     
+            # "speed": 1.25  # Adjust the speed of the speech - apparently this doesn't work
+        }
+        headers = {
+            "xi-api-key": os.getenv('ELEVENLABS_API_KEY'),
+            "Content-Type": "application/json"
+        }
+
+        tony_response = requests.request("POST", url, json=payload, headers=headers)
+        print("received tony response")
+        if tony_response.status_code == 200:
+            # Extract binary audio content
+            audio_content = tony_response.content
+            # Encode as base64
+            audio_base64 = base64.b64encode(audio_content).decode('utf-8')
+            # Emit the base64-encoded audio content via SocketIO
+            socketio.emit('audio_response', {'data': audio_base64})
+
+  
+        else:
+            print("Failed to get audio from ElevenLabs:", tony_response.status_code)
+
     except Exception as e:
         text = f"An error occurred: {e}"
 
